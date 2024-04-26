4ec1acb93fc8f1a7e6b29daf1f5e76834a4925e8
Alyssa Dai <alyssa.ydai@gmail.com>
2024-04-25 22:14:14 -0400

    accept dict request body and add basic error catching

--- app/api/routers/openneuro.py
+++ app/api/routers/openneuro.py
@@ -1,48 +1,49 @@
 import base64
 import json
 import os
-import pathlib
+from typing import Annotated
 
 import requests
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
-MY_PAT = os.environ.get("MY_PAT")
+# TODO: Error out when PAT doesn't exist in environment
+GH_PAT = os.environ.get("GH_PAT")
 
 router = APIRouter(prefix="/openneuro", tags=["openneuro"])
 
 
 @router.put("/upload")
-async def upload():
-    path = pathlib.Path("app/api/routers/example_synthetic.json")
-
-    url = "https://api.github.com/repos/neurobagel/neurobagel_examples/contents/data-upload/example_synthetic.json"
+async def upload(data_dictionary: Annotated[dict, Body()]):
+    # TODO: Populate URL based on path parameter
+    url = "https://api.github.com/repos/neurobagel/test_json/contents/example_synthetic.json"
     headers = {
         "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer " + MY_PAT,
+        "Authorization": "Bearer " + GH_PAT,
         "X-GitHub-Api-Version": "2022-11-28",
     }
 
-    existing_file = requests.get(url, headers=headers)
-    existing_file_sha = existing_file.json()["sha"]
-
-    with open(path, "r") as f:
-        new_data = json.load(f)
+    # TODO: If not exists, create a new file instead
+    target_file = requests.get(url, headers=headers)
+    target_file_sha = target_file.json()["sha"]
 
     # Validation here
 
-    # Now we assume the validation is done, so we b64 encoding
+    # Now we assume the validation is done, so we can convert the dict to a JSON string and then to base64
     new_data_b64 = base64.b64encode(
-        json.dumps(new_data, indent=4).encode("utf-8")
+        # TODO: Make indentation dynamic
+        json.dumps(data_dictionary, indent=4).encode("utf-8")
     ).decode("utf-8")
 
     payload = {
-        "message": "now everything is correct",
+        "message": "[bot] Updating participants.json",
         "content": new_data_b64,
-        "sha": existing_file_sha,
+        "sha": target_file_sha,
     }
 
     # Need to `json.dumps` first b/c the default form encoding is not liked by the GitHub API
     response = requests.put(url, headers=headers, data=json.dumps(payload))
-    print(response)
+
+    if not response.ok:
+        return {"error": response.content}
 
     return {"message": "Finished uploading to OpenNeuro-JSONLD."}
