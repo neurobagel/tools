8e7ca05adac840151fc9a5b0dd65c063ed68d1fb
Alyssa Dai <alyssa.ydai@gmail.com>
2024-04-26 20:07:07 -0400

    update comments

--- app/api/models.py
+++ app/api/models.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel
 
 
+# NOTE: Because we have multiple successful response models, we need extra="forbid" to
+# ensure that instances of SuccessfulUploadWithWarning are not unintentionally
+# converted/filtered to the parent SuccessfulUpload by FastAPI when returning a response.
 class SuccessfulUpload(BaseModel, extra="forbid"):
     """Data model for a response to a successful upload of a file."""
 
     message = "Successfully uploaded file to OpenNeuro-JSONLD."
+    # TODO: Get rid of contents. This doesn't preserve indentation anyways and so is not very useful.
     contents: dict
 
 
 class SuccessfulUploadWithWarning(SuccessfulUpload):
     """Data model for a response to a successful upload of a file with warnings."""
 
-    contents: dict
     warning: str
 
 
