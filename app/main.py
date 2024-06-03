dea1b718c2d4ff0565bad4d125006afffae6d2f5
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2024-06-03 12:11:10 -0400

    [MNT] Enabled CORS (#20)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from .api.routers import openneuro
 
 app = FastAPI(default_response_class=ORJSONResponse)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(openneuro.router)
 
