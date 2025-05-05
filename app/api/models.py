2a523ef31cca1ca389ed54d4f09d67318865af57
Alyssa Dai <alyssa.ydai@gmail.com>
2025-05-05 14:18:56 -0400

    [FIX] Validate the format of provided GitHub usernames (#56)
    * add regex validation for gh_username
    
    * test gh_username validation

--- app/api/models.py
+++ app/api/models.py
@@ -1,4 +1,17 @@
-from pydantic import BaseModel
+import re
+
+from fastapi import HTTPException
+from pydantic import BaseModel, validator
+
+GH_USERNAME_REGEX = re.compile(
+    r"""
+    ^(?!.*--)            # no consecutive hyphens
+    [A-Za-z0-9]          # first character must be alphanumeric
+    ([A-Za-z0-9-]{0,37}  # start of optional group of up to 37 alnum characters or hyphens (1-39 char total)
+    [A-Za-z0-9])?$       # must end with an alphanumeric character
+""",
+    re.VERBOSE,
+)
 
 
 class Contributor(BaseModel):
@@ -9,6 +22,24 @@ class Contributor(BaseModel):
     affiliation: str | None = None
     gh_username: str | None = None
     changes_summary: str
+
+    @validator("gh_username")
+    def valid_github_username(cls, v):
+        """
+        Ensure that the GitHub username looks valid according to GitHub's rules -
+        this also ensures that the resulting branch name generated for the PR is valid:
+        https://docs.github.com/en/enterprise-cloud@latest/admin/managing-iam/iam-configuration-reference/username-considerations-for-external-authentication#about-username-normalization
+
+        NOTE: We currently don't actually check whether the user exists, which is possible but would require
+        a separate API call (e.g., https://pygithub.readthedocs.io/en/stable/utilities.html?highlight=githubexception#raw-requests)
+        (However, the regex would still be needed, as bot usernames are usually not valid GH usernames)
+        """
+        if v and not GH_USERNAME_REGEX.fullmatch(v):
+            raise HTTPException(
+                status_code=422,
+                detail="GitHub username (gh_username) contains invalid characters. Please double check that you have entered a valid GitHub username.",
+            )
+        return v
 
 
 # NOTE: Because we have multiple successful response models, we need extra="forbid" to
