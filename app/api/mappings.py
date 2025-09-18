55cc4e79c39b9bc298742a24cef82d58dd1a5f68
Alyssa Dai <alyssa.ydai@gmail.com>
2025-09-18 16:43:28 -0400

    [MNT] Upgrade data dictionary schema and validation for compatibility with `bagel>=0.8.0` (#68)
    * update data dictionary model
    
    * update test data
    
    * update the data dict validation to match the latest validation in the bagel CLI

--- app/api/mappings.py
+++ app/api/mappings.py
@@ -1,10 +1,10 @@
+# This file is adapted from https://github.com/neurobagel/bagel-cli/blob/main/bagel/mappings.py
+# and contains only the mappings used for Neurobagel data dictionary validation.
+
 from collections import namedtuple
 
 Namespace = namedtuple("Namespace", ["pf", "url"])
-COGATLAS = Namespace("cogatlas", "https://www.cognitiveatlas.org/task/id/")
 NB = Namespace("nb", "http://neurobagel.org/vocab/")
-NCIT = Namespace("ncit", "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#")
-SNOMED = Namespace("snomed", "http://purl.bioontology.org/ontology/SNOMEDCT/")
 
 NEUROBAGEL = {
     "participant": NB.pf + ":ParticipantID",
@@ -12,6 +12,6 @@ NEUROBAGEL = {
     "sex": NB.pf + ":Sex",
     "age": NB.pf + ":Age",
     "diagnosis": NB.pf + ":Diagnosis",
-    "healthy_control": NCIT.pf + ":C94342",
+    "subject_group": NB.pf + ":SubjectGroup",
     "assessment_tool": NB.pf + ":Assessment",
 }
